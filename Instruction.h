cab5c53576ce6d0ef0bb45a3fbf21a79ffab75ca
houstonlucas <houstonlucas@nevada.unr.edu>
2018-07-02 22:42:12 -0700

    Created an instruction class.

---
+++ Instruction.h
@@ -0,0 +1,14 @@
+//
+// Created by namesnonames on 7/1/18.
+//
+
+#ifndef CHIP8EMULATOR_INSTRUCTION_H
+#define CHIP8EMULATOR_INSTRUCTION_H
+
+
+class Instruction {
+
+};
+
+
+#endif //CHIP8EMULATOR_INSTRUCTION_H
