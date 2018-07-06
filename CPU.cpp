6875adb586e41ff6b48327fd38a8d29d6b1b0709
houstonlucas <houstonlucas@nevada.unr.edu>
2018-07-06 13:32:19 -0700

    Implemented most opcodes.

--- CPU.cpp
+++ CPU.cpp
@@ -3,3 +3,199 @@
 //
 
 #include "CPU.h"
+
+void CPU::processInstruction(uint8_t *memory, Screen *screen) {
+    uint8_t leftNibbleMask = 0xf0;
+    uint8_t rightNibbleMask = 0x0f;
+    uint8_t leftByte = memory[programCounter];
+    uint8_t rightByte = memory[programCounter+1];
+
+    uint16_t highAddress =(uint16_t)((leftByte & rightNibbleMask) << 8) ;
+    uint16_t address = (highAddress) ^ rightByte;
+
+    uint8_t x = leftByte & rightNibbleMask;
+    uint8_t y = rightByte & leftNibbleMask;
+    uint8_t k = rightByte & rightNibbleMask;
+
+    switch((leftByte & leftNibbleMask) >> 4){
+        case 0x0:
+            // CLS and RET
+            if(rightByte == 0xE0){
+                screen->clearScreen();
+            }else if(rightByte == 0xEE){
+                //TODO: RET
+            }else{
+                //SYS addr???
+            }
+            std::cout << "0x0" << std::endl;
+            break;
+        case 0x1:
+            // JP address
+            programCounter = address;
+            std::cout << "0x1" << std::endl;
+            break;
+        case 0x2:
+            // CALL address
+            std::cout << "0x2" << std::endl;
+            break;
+        case 0x3:
+            // SE Vx kk
+            if(V[x] == rightByte){
+                programCounter += 2;
+            }
+            std::cout << "0x3" << std::endl;
+            break;
+        case 0x4:
+            // SNE Vx kk
+            if(V[x] != rightByte){
+                programCounter += 2;
+            }
+            std::cout << "0x4" << std::endl;
+            break;
+        case 0x5:
+            // SE Vx Vy 0
+            if(V[x] == V[y]){
+                programCounter += 2;
+            }
+            std::cout << "0x5" << std::endl;
+            break;
+        case 0x6:
+            // LD Vx kk
+            V[x] = rightByte;
+            std::cout << "0x6" << std::endl;
+            break;
+        case 0x7:
+            // Add Vx kk
+            V[x] += rightByte;
+            std::cout << "0x7" << std::endl;
+            break;
+        case 0x8:
+            // Various Math
+            process0x8(x, y, k);
+            std::cout << "0x8" << std::endl;
+            break;
+        case 0x9:
+            // SNE Vx Vy 0
+            if(V[x] != V[y]){
+                programCounter += 2;
+            }
+            std::cout << "0x9" << std::endl;
+            break;
+        case 0xa:
+            // LD I address
+            I = address;
+            std::cout << "0xa" << std::endl;
+            break;
+        case 0xb:
+            // JP V0 address
+            programCounter = V[0] + address;
+            std::cout << "0xb" << std::endl;
+            break;
+        case 0xc:
+            // RND Vx kk
+            V[x] = ((uint8_t) random()) & rightByte;
+            std::cout << "0xc" << std::endl;
+            break;
+        case 0xd:
+            // DRW Vx Vy k
+            screen->drawSprite(memory, I, k, V[x], V[y]);
+            std::cout << "0xd" << std::endl;
+            break;
+        case 0xe:
+            // Skip instructions
+            std::cout << "0xe" << std::endl;
+            break;
+        case 0xf:
+            // Various flag operations
+            process0xF(memory, x, rightByte);
+            std::cout << "0xf" << std::endl;
+            break;
+        default:
+            //NOP I guess
+            // ¯\_(ツ)_/¯
+            break;
+    }
+    programCounter += 2;
+}
+
+int CPU::getProgramCounter() const {
+    return programCounter;
+}
+
+void CPU::process0x8(uint8_t x, uint8_t y, uint8_t k) {
+    //TODO: set flags on instructions
+    switch(k){
+        case 0:
+            V[x] = V[y];
+            break;
+        case 1:
+            V[x] = V[x] | V[y];
+            break;
+        case 2:
+            V[x] = V[x] & V[y];
+            break;
+        case 3:
+            V[x] = V[x] ^ V[y];
+            break;
+        case 4:
+            V[x] = V[x] + V[y];
+            break;
+        case 5:
+            V[x] = V[x] - V[y];
+            break;
+        case 6:
+            V[x] = V[x] >> 1;
+            break;
+        case 7:
+            V[x] = V[y] - V[x];
+            break;
+        case 0xe:
+            V[x] = V[x] << 1;
+            break;
+        default:
+            break;
+    }
+}
+
+void CPU::process0xF(uint8_t *memory, uint8_t x, uint8_t rightByte) {
+    switch(rightByte){
+        case 0x07:
+            V[x] = delayTimer;
+            break;
+        case 0x0a:
+            //TODO: input stuff
+            break;
+        case 0x15:
+            delayTimer = V[x];
+            break;
+        case 0x18:
+            soundTimer = V[x];
+            break;
+        case 0x1e:
+            I += V[x];
+            break;
+        case 0x29:
+            //TODO: load sprite Vx address;
+            loadSpriteAddress(x);
+            break;
+        case 0x33:
+            //TODO: BCD encoding into I, I+1, I+2 address
+            break;
+        case 0x55:
+            for(int i = 0; i <= x; i++){
+                memory[I+i] = V[i];
+            }
+            break;
+        case 0x65:
+            for(int i = 0; i <= x; i++){
+                V[i] = memory[I+i];
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+void CPU::loadSpriteAddress(uint8_t x){
+    I = (uint16_t) SPRITE_ADDRESSES[V[x]];
+}
--- CPU.h
+++ CPU.h
@@ -7,11 +7,12 @@
 
 
 #include <stdint-gcc.h>
+#include "Screen.h"
 
 class CPU {
 private:
-    int programCounter;
     int stackPointer;
+    int programCounter = 0x200;
 
 
     uint16_t I;
@@ -19,6 +20,21 @@ private:
     uint8_t delayTimer;
     uint8_t soundTimer;
 
+    void process0x8(uint8_t x, uint8_t y, uint8_t k);
+    void process0xF(uint8_t *memory, uint8_t x, uint8_t rightByte);
+
+public:
+
+    const int SPRITE_ADDRESSES[16] = {0x0, 0x5, 0xa, 0xf,
+                                    0x14, 0x19, 0x1e, 0x23,
+                                    0x28, 0x2d, 0x32, 0x37,
+                                    0x3c, 0x41, 0x46, 0x4b
+    };
+    void processInstruction(uint8_t *memory, Screen *screen);
+
+    void loadSpriteAddress(uint8_t x);
+
+    int getProgramCounter() const;
 };
 
 
